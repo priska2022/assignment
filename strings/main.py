99e89a0def9ab6c19ff79578b7f863129000d7af
pbeens <pbeens@MacOS.local>
2022-07-21 14:15:09 +0200

    code aangepast

--- strings/main.py
+++ strings/main.py
@@ -4,9 +4,9 @@ __human_name__ = 'strings'
 
 # Add your code after this line
 # Finale UEFA Euro 1988
-# part 1
-# Create a variable for each player who scored
 
+# # part 1
+# Create a variable for each player who scored
 player_0 = "Ruud Gullit"
 player_1 = "Marco van Basten"
 
@@ -16,36 +16,37 @@ goal_1 = 54
 
 # Using the +-operator, create a string that reports on who scored when
 
-scorers = player_0 + " " + str(goal_0) + " " + player_1 + " " + str(goal_1)
+scorers = player_0 + " " + str(goal_0) + ", " + player_1 + " "+ str(goal_1)
+print(scorers)
 
 # Use f-strings or the +-operator to create a single string with information about who scored when
 
-report = f"{player_0} scored in the {goal_0}nd minute \n{player_1} scored in the {goal_1}th minute "
-#print(report)
+report = f"{player_0} scored in the {goal_0}nd minute\n{player_1} scored in the {goal_1}th minute"
+print(report)
 
 # part 2
 # Choose a player that played in the soccer match and store his name as a string
 
 player = 'Marco van Basten'
-first_name = player[0:5]
-player.find('Marco')
+
+#use slicing and the find-method to isolate the player's first name
+first_name = player[:player.find(' ')]
 #print(first_name)
+#print(player.find(' '))
+#print(player[:player.find(' ')])
 
+#use find methode, slicing and len to isolate the player's last name
+last_name_len = len(player[:player.find(' ',)+5])
+print(last_name_len)
 
-player.find('van Basten')
-last_name= player[6:]
-#print(last_name)
+name_short = player[:1] + '.' + player[player.find(' '):]
+print(name_short)
 
-last_name_len = len(last_name)
-#print(last_name_len)
+chant = (first_name + '! ') * 5
+print(chant)
 
-name_short = 'M. van Basten'
-
-chant = 'Marco!' * 5
-#print(chant)
-
-good_chant = chant != 5
-#print(good_chant)
+good_chant = chant != ' '
+print(good_chant)
 
 
 
